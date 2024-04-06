2189a6f6db0e744cf0e2afc3aa068142dc06035d
shash123-d <88714649+shash123-d@users.noreply.github.com>
2024-04-06 16:50:04 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,56 +1,25 @@
-from tkinter import *
-from pacman import pacman
-from ghost import ghost
+import asyncio
+import pygame
 
-C = 36
-X, Y = 20, 15
+pygame.init()
 
-
-class Main:
-    def __init__(self):
-        self.root = Tk()
-        self.root.title("Pacman")
-        self.canvas = Canvas(self.root, height=C*Y, width=C*X)
-        self.canvas.grid(row=0, column=0)
-
-        for i in range(X):
-            for j in range(Y):
-                self.canvas.create_rectangle(i*C, j*C, (i*C)+C, (j*C)+C, fill='black', outline='grey')
-
-        self.pacman = pacman(0, 0, C, X, Y, self.canvas, self.root)
-        self.ghosts = []
-        self.ghosts.append(ghost(0, 3, C, X, Y, "red", self.canvas))
-        self.ghosts.append(ghost(0, 5, C, X, Y, "orange", self.canvas))
-        self.ghosts.append(ghost(0, 10, C, X, Y, "cyan", self.canvas))
-        self.ghosts.append(ghost(0, 14, C, X, Y, "pink", self.canvas))
-
-        self.root.bind('<Up>', self.pacman.up)
-        self.root.bind('<Left>', self.pacman.left)
-        self.root.bind('<Right>', self.pacman.right)
-        self.root.bind('<Down>', self.pacman.down)
-        self.root.bind('w', self.pacman.up)
-        self.root.bind('a', self.pacman.left)
-        self.root.bind('d', self.pacman.right)
-        self.root.bind('s', self.pacman.down)
-
-        self.run()
-
-    def run(self):
-
-        while not self.checkdead():
-            self.pacman.update()
-            for ghost in self.ghosts:
-                ghost.update()
-
-        self.root.mainloop()
-
-    def checkdead(self):
-        for ghost in self.ghosts:
-            if (ghost.x == self.pacman.x) and (ghost.y == self.pacman.y):
-                return True
-
-        return False
+screen = pygame.display.set_mode([500, 500])
 
 
-if __name__ == "__main__":
-    obj = Main()
+async def main():
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((255, 255, 255))
+
+        pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+asyncio.run(main())
